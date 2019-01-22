72ab9e7be03277cee3f76dc0445829b2a37b1ab8
Kamil <gryxon@gmail.com>
2019-01-22 21:48:12 +0100

    Add OSNR control.

--- soib.m
+++ soib.m
@@ -1,8 +1,8 @@
-function [OSNR, Q] = soib(PN, deltaN, deltaFo, f, P1, EX)
+function [OSNR, Q] = soib(PN, deltaN, deltaFo, f, P1, EX, step)
 if deltaN > deltaFo
   deltaN = deltaFo
 end;
-step = 0.00001;
+step = deltaN/1000;
 B = [f-deltaN/2:step:f+deltaN/2];
 Bod = [f-deltaFo/2:step:f+deltaFo/2];
 Bextended = [f-deltaN:step:f+deltaN];
@@ -15,15 +15,15 @@ signal0 = zeros(1, length(B));
 signal0(int32(length(B)/2)) = P0;
 noise = noise_fun(B);
 
-left_filtered_freq = find(abs(Bextended - (f-deltaFo/2)) < 0.000001);
-right_filtered_freq = find(abs(Bextended - (f+deltaFo/2)) < 0.000001);
+left_filtered_freq = find(abs(Bextended - (f-deltaFo/2)) < 0.0001);
+right_filtered_freq = find(abs(Bextended - (f+deltaFo/2)) < 0.0001);
 
 asease_od = conv(noise, noise)(left_filtered_freq:right_filtered_freq);
 signal1_od = conv(signal1, noise)(left_filtered_freq:right_filtered_freq);
 signal0_od = conv(signal0, noise)(left_filtered_freq:right_filtered_freq);
 
-sigma1 = sqrt(sum((signal1_od + asease_od).*step));
-sigma0 = sqrt(sum((signal0_od + asease_od).*step));
+sigma1 = sqrt(trapz(Bod, signal1_od + asease_od));
+sigma0 = sqrt(trapz(Bod, signal0_od + asease_od));
 
 OSNR = 10*log10(P1/(deltaN*PN));
 Q = (P1 - P0) / (sigma1 + sigma0);
--- soib_gui.m
+++ soib_gui.m
@@ -1,12 +1,18 @@
-prompt = {'Receiver filter bandwidth [Thz]', 'Noises bandwidth [Thz]', 'Frequency [Thz]', 'P1 [mW]', 'EX'};
-definput = {0.0175, 0.0175, 229, 1, 50};
-[deltaFo, deltaN, f, P1, EX] = inputdlg(prompt, 'Input', 1, definput){:};
+prompt = {'Receiver filter bandwidth [Thz]', 'Noises bandwidth [Thz]', 'Frequency [Thz]', 'P1 [mW]',
+          'EX', 'OSNR_min [db]', 'OSNR_max [db]', 'OSNRs counts'};
+definput = {0.0175, 0.0175, 229, 1, 50, 15, 35, 500};
+[deltaFo, deltaN, f, P1, EX, OSNR_min, OSNR_max, counts] = inputdlg(prompt, 'Input', 1, definput){:};
 deltaFo = str2num(deltaFo);
 deltaN = str2num(deltaN);
 f = str2num(f);
 P1 = str2num(P1);
 EX = str2num(EX);
-Pns = 0.02:0.001:1;
+OSNR_min = str2num(OSNR_min);
+OSNR_max = str2num(OSNR_max);
+Pn_min = P1/((10^(OSNR_min/10)) * deltaN);
+Pn_max = P1/((10^(OSNR_max/10)) * deltaN);
+counts = str2num(counts);
+Pns = linspace(Pn_min, Pn_max, counts);
 OSNRs = zeros(1, length(Pns));
 Qs = zeros(1, length(Pns));
 for i=1:length(Pns)
