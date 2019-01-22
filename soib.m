462a37aa945e88c229b6e53e52393df68600d267
Kamil <kkamilstasiakk@gmail.com>
2019-01-22 11:16:39 +0100

    moce

--- soib.m
+++ soib.m
@@ -5,6 +5,9 @@ step = 0.00001;
 B = [f-deltaN/2:step:f+deltaN/2];
 P1 = 1000;
 PN = 2;
+S = 0.7; %A/W
+R = 100; %ohm
+EX = 1000;
 noise_fun = @(x) PN*ones(1, length(x));
 
 signal = zeros(1, length(B));
@@ -21,3 +24,11 @@ signalase1 = conv(signal, noise);
 figure(2);
 title('signal(1) - ase');
 plot(signalase1)
+
+I1 = P1 * S;
+P1el = I1*I1*R;
+P0 = P1/EX;
+I0 = P0 * S;
+P0el = I0*I0*R;
+
+OSNR = P1/deltaN*PN;
